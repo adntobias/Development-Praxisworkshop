d898b1c431113e1cb9a02bcbfc578b15b61b1846
ADNTobi <tobias.pitzschke@adn.de>
2022-07-27 20:01:01 +0200

    smol fixes

--- Frontend/Pages/ImageGallery.cshtml.cs
+++ Frontend/Pages/ImageGallery.cshtml.cs
@@ -13,7 +13,7 @@ using Development_Praxisworkshop.Helper;
 namespace Development_Praxisworkshop.Pages
 {
     //[AllowAnonymous]
-    [Authorize(Roles = ("3b166cfb-21ff-4ba7-9460-819d066d9b94"))] // Win10E3License
+    [Authorize(Roles = ("a6d89943-0f07-4eec-aa1c-2f9ef44fb5c5"))] // testgrp
     public class GalleryModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
